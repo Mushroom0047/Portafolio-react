532f4eed8ae5ddb7976ae89345d590d68c2364fe
MushrooM <hectorvaldesm47@gmail.com>
2022-05-10 22:11:17 -0400

    Changes in font and something in navbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
 import logoNavMenu from '../../img/logo/mushroom-isotipo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faSkull} from '@fortawesome/free-solid-svg-icons';
+import {faSkull, faBurger} from '@fortawesome/free-solid-svg-icons';
 import '../Header/header.scss';
 import { Link } from 'react-router-dom';
 
@@ -15,12 +15,13 @@ const navMenuLinks = {
 }
 
 const Header = () => {
+    const [isMobile, setIsMobile] = useState(false);
   return (
     <div className='box__navmenu__container'>
         <div className='box__navmenu__logo'>
             <Link to={navMenuLinks.home}><img alt='' src={logoNavMenu}/></Link>
         </div>
-        <div className='box__navmenu__buttons'>
+        <div className= {isMobile ? 'box__navmenu__buttons__mobile': 'box__navmenu__buttons'}>
             <ul>
                 <li><Link to={navMenuLinks.sobremi}>Sobre Mí</Link></li>
                 <li><Link to={navMenuLinks.proyectos}>Proyectos</Link></li>
@@ -28,6 +29,9 @@ const Header = () => {
                 <li><Link to={navMenuLinks.blog}>Blog</Link></li>
                 <li><Link to={navMenuLinks.login}><FontAwesomeIcon icon={faSkull} className='navmenu__icon__user'/></Link></li>
             </ul>
+            <button className='box__menu__icon' onClick={()=>{setIsMobile(!isMobile)}}>
+                {isMobile ? <i className={faBurger}></i>: <i className={faSkull}></i>}
+            </button>
         </div>
     </div>
   )
